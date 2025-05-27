04b8e793e0a5c1adb6ed731534776591a022add8
majiputraa <m.ajiputraa12@gmail.com>
2025-05-27 17:21:21 +0700

    NotFoundPage1

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { TfiFaceSad } from "react-icons/tfi";
+
+function NotFoundPage() {
+  return (
+    <>
+    <div className="flex justify-center items-center">
+    <TfiFaceSad />
+    <h1>| 404 NOT FOUND</h1>
+    </div>
+    </>
+  );
+}
+
+export default NotFoundPage;
