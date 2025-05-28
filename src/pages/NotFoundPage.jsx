27533eeba9e603f487353e02d686a1e075d5ab45
majiputraa <m.ajiputraa12@gmail.com>
2025-05-28 16:36:44 +0700

    NotFoundPage Update

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { TfiFaceSad } from "react-icons/tfi";
 import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
 
 function NotFoundPage() {
   return (
     <>
       <NavBar />
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center h-screen text-3xl">
         <TfiFaceSad />
-        <h1>| 404 NOT FOUND</h1>
+        <h1 className="mt-5">404 NOT FOUND</h1>
+        <Link  to="/">
+          <button className="btn btn-outline mt-5">Go Back</button>
+        </Link>
       </div>
     </>
   );
