461f91c55644587e983cbc25318ceb886f9539de
Shin Hakuro <muhammadrizky040206@gmail.com>
2025-05-28 17:08:27 +0700

    Firebase contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { db } from "../Firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 function ContactPage() {
   return (
