75a9423a96de6da7eb110b37796ac68596b87b67
R. M. Amir Arrizqi <rmamir1408@gmail.com>
2025-05-30 08:23:10 +0700

    h

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -161,7 +161,7 @@ function AboutHero() {
                       d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                     />
                   </svg>
-                  Lihat Menu Kami
+                  <a href="product">Lihat Menu Kami</a>
                   <svg
                     className="w-5 h-5 group-hover:translate-x-1 transition-transform"
                     fill="none"
